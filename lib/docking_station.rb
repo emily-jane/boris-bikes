2acfbec134d105c9c70843e8f6cfcc211616366d
Emily Carson <emilyjanecarson@gmail.com>
2015-07-21 16:58:33 +0100

    Updating challenge 10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,11 @@
+require_relative "Bike"
+
 class DockingStation
 
 def release_bike
+
+	Bike.new
+
 end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,10 @@ require "docking_station"
 describe DockingStation do
 	it { is_expected.to respond_to :release_bike }
 end
+
+describe DockingStation do
+	it "expects bike to be working" do
+		bike = subject.release_bike
+		expect(bike).to respond_to :working?
+	end
+end
